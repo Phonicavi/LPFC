1457ecce407b6d1f3390cabe777162eec120e197
Phonicavi <mardpazin7san@hotmail.com>
2016-02-06 02:00:24 +0800

    2016-Feb.6th
    修改了路由中的重要错误，初步添加了MVC模式中的Controller :)

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,9 @@
+<?php
+
+class HomeController extends BaseController {
+
+    public function home()
+    {
+        echo "Controller Success...";
+    }
+}
--- app/routes.php
+++ app/routes.php
@@ -2,8 +2,20 @@
 
 use LPFCRouter\Hermes\Route;
 
+
+/**
+ * both Route::get('welcome', 'function');
+ * and Route::get('/welcome', 'function');
+ * are OK!!
+ */
 Route::get('welcome', function() {
     echo 'welcome..';
+});
+
+Route::get('home', 'HomeController@home');
+
+Route::post('/(:all)', function($foo) {
+    echo "Undefined route:<br>".$foo;
 });
 
 Route::dispatch();
